9bf4f75bd1fe6c3d42763b82329510b003e3e439
8ehemoth <dongje226@gmail.com>
2026-03-25 16:57:21 +0900

    Add files via upload

---
+++ week4/p2026-5/p2026-5/smTranspose.c
@@ -0,0 +1,48 @@
+#include "smTranspose.h"
+
+void smTranspose(term a[], term b[]) {
+	int m, n, v, i, j, p;
+	m = a[0].row;	// 희소 행렬 a의 행 수
+	n = a[0].col;	// 희소 행렬 a의 열 수
+	v = a[0].value; // 희소 행렬 a에서 0이 아닌 원소 수
+	b[0].row = n;	// 전치 행렬 b의 행 수
+	b[0].col = m;	// 전치 행렬 b의 열 수
+	b[0].value = v;	// 전치 행렬 b의 원소 수
+	if (v > 0) {	// 0이 아닌 원소가 있는 경우에만 전치 연산 수행
+		p = 1;
+		for (i = 0; i < n; i++)			// 희소 행렬 a의 열별로 전치 반복 수행
+			for (j = 1; j <= v; j++)	// 0이 아닌 원소 수에 대해서만 반복 수행
+				if (a[j].col == i) {	// 현재의 열에 속하는 원소가 있으면 b[]에 삽입
+					//새롭게 작성
+					b[p].row = a[j].col;
+					b[p].col = a[j].row;
+					b[p].value = a[j].value;
+					p++;
+				}
+	}
+}
+
+void smPrint(term a[]) {
+	int row, col, val, i, j, k, ijvalue;
+	row = a[0].row;
+	col = a[0].col;
+	val = a[0].value;
+
+	for (i = 0; i < row; i++) {
+		for (j = 0; j < col; j++) {
+			// (i, j)번째 원소를 찾아서 출력
+			ijvalue = 0;
+			for (k = 1; k <= val; k++) {
+				if (a[k].row == i && a[k].col == j) {
+					ijvalue = a[k].value;
+					break;
+				}
+			}
+			printf("%5d", ijvalue);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+}
+
+
